c6c2a7ae545faa777d0624e5ae542a17ccf34e51
mrmiagi <gerhard.alfanz@magora.com>
2017-02-16 13:44:10 +0100

    Added some return values when writing to the TTY stream

--- src/Stream/DockerTtyStream.php
+++ src/Stream/DockerTtyStream.php
@@ -17,16 +17,17 @@ class DockerTtyStream
      * Send input to the container
      *
      * @param string $data Data to send
+     * @return int|bool Amount of bytes or false on write error
      */
     public function write($data)
     {
-        $this->socketWrite($data);
+        return $this->socketWrite($data);
     }
 
     public function read($maxBytes = 500000)
     {
         if (!is_resource($this->socket) || feof($this->socket)) {
-            return null;
+            return false;
         }
 
         return fread($this->socket, $maxBytes);
